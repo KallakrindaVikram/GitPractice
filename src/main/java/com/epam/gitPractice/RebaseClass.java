e3ee6bfaa64b4589bd60c8aeb958e765cf118227
Kallakrinda Vikram <kallakrinda_vikram@epam.com>
2025-03-18 23:13:11 +0530

    cherry 1

--- src/main/java/com/epam/gitPractice/RebaseClass.java
+++ src/main/java/com/epam/gitPractice/RebaseClass.java
@@ -1,4 +1,9 @@
 package com.epam.gitPractice;
 
+import org.w3c.dom.ls.LSOutput;
+
 public class RebaseClass {
+    public static void main(String[] args) {
+        System.out.println("Cherry 1");
+    }
 }
