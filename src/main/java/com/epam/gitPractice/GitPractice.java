cea7b193a712c9a67aea20a60693558ba9fd51d5
Kallakrinda Vikram <kallakrinda_vikram@epam.com>
2025-03-18 22:43:25 +0530

    dev changes

--- src/main/java/com/epam/gitPractice/GitPractice.java
+++ src/main/java/com/epam/gitPractice/GitPractice.java
@@ -3,5 +3,6 @@ package com.epam.gitPractice;
 public class GitPractice {
     public static void main(String[] args) {
         System.out.println("This is git practice");
+        System.out.println("This changes are in dev");
     }
 }
