a710467bcb26e1526318070bf0fe946eeca2cbd5
Kallakrinda Vikram <kallakrinda_vikram@epam.com>
2025-03-18 22:56:20 +0530

    made dev changes in master

--- src/main/java/com/epam/gitPractice/GitPracticeHehe.java
+++ src/main/java/com/epam/gitPractice/GitPracticeHehe.java
@@ -1,11 +1,12 @@
 package com.epam.gitPractice;
 
-public class GitPractice {
+public class GitPracticeHehe {
     public static void main(String[] args) {
         System.out.println("This is git practice");
         System.out.println("This changes are in dev");
         System.out.println("This changes are in dev1");
         System.out.println("This changes are in dev2");
         System.out.println("This changes are in dev3");
+        System.out.println("This changes are in dev4 mergeconflict");
     }
 }
