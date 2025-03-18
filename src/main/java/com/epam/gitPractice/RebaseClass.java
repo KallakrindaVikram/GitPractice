418cd5981678e63ded716a6ef49ed6eef3be8832
Kallakrinda Vikram <kallakrinda_vikram@epam.com>
2025-03-18 23:14:51 +0530

    resolved merge conflict

--- src/main/java/com/epam/gitPractice/RebaseClass.java
+++ src/main/java/com/epam/gitPractice/RebaseClass.java
@@ -5,5 +5,7 @@ import org.w3c.dom.ls.LSOutput;
 public class RebaseClass {
     public static void main(String[] args) {
         System.out.println("Cherry 1");
+        System.out.println("Cherry 2");
+        System.out.println("Cherry 3");
     }
 }
